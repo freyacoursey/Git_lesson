d00740f8ef8f197d0f652cc843abe922ec4e3bea
freyacoursey <freyacoursey@hotmail.co.uk>
2024-11-08 09:37:52 +0100

    initial commit

---
+++ R/Script.R
@@ -0,0 +1,20 @@
+## my git script ##
+
+library(usethis)
+
+use_git_config(
+  user.name = "xxx",
+  user.email = "xxx"
+)
+
+# create new token (only needed once previous one has expired)
+#usethis::create_github_token() # takes you to website with token
+#gitcreds::gitcreds_set() # run this then paste token
+
+# create a new git repository
+usethis::use_git()
+
+use_github() # commit files to github
+
+
+
